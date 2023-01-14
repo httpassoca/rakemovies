8130813029ef02a3fad405e95cfdbc77db2a83a1
httpassoca <me@passoca.dev>
2023-01-14 20:06:55 +0000

    feat: inspiration idea

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import AppTitle from './components/Base/AppTitle.vue';
 import Search from './components/Search.vue';
+import Inspiration from './components/Inspiration.vue';
 
 </script>
 
 <template>
-  <div class="container mx-auto flex flex-col justify-center items-center min-h-screen">
-    <AppTitle class="mb-4" />
+  <div class="container mx-auto flex flex-col justify-center items-center min-h-screen gap-8">
+    <AppTitle />
     <Search />
+    <Inspiration />
   </div>
 </template>
 
---
+++ src/components/Inspiration.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p>Need some inspiration? Try searching my favorites: </p>
+  <div>
+    x - x - x
+  </div>
+</template>
+
+<style scoped lang="sass">
+form
+  display: flex
+  gap: 8px
+input
+  background-color: white
+  padding: 6px 12px
+  color: black
+  outline: none
+  border-radius: 4px
+button
+  color: black
+  padding: 6px 12px
+  border-radius: 4px
+</style>
